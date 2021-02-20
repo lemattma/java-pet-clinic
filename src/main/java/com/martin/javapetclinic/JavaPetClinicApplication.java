d240514e3db3e03db957785ac0cc4b1dfc7cd287
Martin Miranda <mmiranda@atlassian.com>
2021-02-20 16:05:58 +1100

    new springboot project

---
+++ src/main/java/com/martin/javapetclinic/JavaPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.martin.javapetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/martin/javapetclinic/JavaPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martin.javapetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
