cbbbb23c0969816f8c478c58d0daeea0bdad9f78
Martin Miranda <mmiranda@atlassian.com>
2021-02-26 22:51:56 +1100

    Added base entity

--- pet-clinic-data/src/main/java/com/martin/javapetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/martin/javapetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.martin.javapetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
 
     private PetType petType;
     private Owner owner;
--- pet-clinic-data/src/main/java/com/martin/javapetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/martin/javapetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.martin.javapetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
