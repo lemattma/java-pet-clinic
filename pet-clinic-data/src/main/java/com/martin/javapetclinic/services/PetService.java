e8615e02037dda6a1ba10eb8aa59c1195cb62e28
Martin Miranda <mmiranda@atlassian.com>
2021-03-04 13:50:10 +1100

    base crud interface

--- pet-clinic-data/src/main/java/com/martin/javapetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/martin/javapetclinic/services/PetService.java
@@ -2,13 +2,5 @@ package com.martin.javapetclinic.services;
 
 import com.martin.javapetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById(Long id );
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
--- pet-clinic-data/src/main/java/com/martin/javapetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/martin/javapetclinic/services/VetService.java
@@ -2,13 +2,5 @@ package com.martin.javapetclinic.services;
 
 import com.martin.javapetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id );
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
